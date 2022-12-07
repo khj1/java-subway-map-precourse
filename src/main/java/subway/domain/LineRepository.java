f864c7104a22beac4b1cb7a463a0642fdf479bf1
khj1 <dhepd8849@gmail.com>
2022-12-07 20:16:42 +0900

    feat: 노선 이름을 추출하는 기능 추가

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -4,14 +4,23 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class LineRepository {
+    
     private static final String DUPLICATED_LINE_MESSAGE = "해당 노선은 이미 존재합니다.";
+    private static final String LINE_NAME_NOT_FOUND_MESSAGE = "해당 노선은 존재하지 않습니다.";
 
     private static final List<Line> lines = new ArrayList<>();
 
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
+    }
+
+    public static List<String> lineNames() {
+        return lines.stream()
+                .map(Line::getName)
+                .collect(Collectors.toList());
     }
 
     public static void addLine(Line line) {
@@ -38,6 +47,6 @@ public class LineRepository {
         return lines().stream()
                 .filter(line -> line.isSameName(name))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("해당 노선은 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException(LINE_NAME_NOT_FOUND_MESSAGE));
     }
 }
