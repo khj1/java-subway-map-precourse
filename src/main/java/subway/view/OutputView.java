e144fe278e2a46014bee280e905dc1add5a8cd08
khj1 <dhepd8849@gmail.com>
2022-12-07 15:13:10 +0900

    feat: 메인 메뉴를 출력하는 기능 추가

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -2,6 +2,9 @@ package subway.view;
 
 import subway.domain.StationRepository;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class OutputView {
 
     private static final String INFO_PREFIX = "[INFO] %s";
@@ -9,5 +12,15 @@ public class OutputView {
     public void printStations() {
         StationRepository.stations()
                 .forEach(station -> System.out.printf(INFO_PREFIX, station));
+    }
+
+    public void printMain() {
+        System.out.println(makeMainMenu());
+    }
+
+    private String makeMainMenu() {
+        return Arrays.stream(MainCommand.values())
+                .map(mainCommand -> String.format("%s. %s", mainCommand.getCommand(), mainCommand.getDescription()))
+                .collect(Collectors.joining(System.lineSeparator()));
     }
 }
